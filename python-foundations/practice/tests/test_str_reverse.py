c08a442f319dfbf3717a6e7d060e6b86fddea2b5
jay <l13103768482@gmail.com>
2026-05-29 16:36:43 +0800

    Add four tests as today's homework

--- python-foundations/practice/tests/test_str_reverse.py
+++ python-foundations/practice/tests/test_str_reverse.py
@@ -4,3 +4,10 @@ def test_str_reverse():
     result = str_reverse("abcdef")
     assert result == "fedcba"
     
+    assert str_reverse(
+        ""
+    )==""
+
+    assert str_reverse(
+        "a"
+    )=="a"
