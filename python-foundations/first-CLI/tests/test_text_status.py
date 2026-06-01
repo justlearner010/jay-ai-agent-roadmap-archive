75ab2d039c1f7145d4d14fefd89d8779e7e59db4
jay <l13103768482@gmail.com>
2026-06-01 17:01:47 +0800

    add new tests in test_text_status.py

--- python-foundations/first-CLI/tests/test_text_status.py
+++ python-foundations/first-CLI/tests/test_text_status.py
@@ -1,3 +1,4 @@
+import pytest
 from text_status import TextStats
 
 
@@ -16,3 +17,21 @@ def test_all(tmp_path):
     assert stats.word_check() == 5
     assert stats.digit_check() == 6
     assert stats.space_check() == 4
+    
+def test_empty_file(tmp_path):
+    file = tmp_path / "empty.txt"
+    file.write_text("")
+
+    stats = TextStats(str(file))
+
+    assert stats.line_check() == 0
+    assert stats.word_check() == 0
+    assert stats.space_check() == 0
+    assert stats.digit_check() == 0
+
+
+def test_file_not_found():
+    stats = TextStats("not_exist.txt")
+
+    with pytest.raises(FileNotFoundError):
+        stats.word_check()
